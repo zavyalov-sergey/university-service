7c8ef15c179b3d8f02a921aed5843d0e63ae9618
Sergey Zavyalov <i@fatallooo.ru>
2024-07-29 15:14:20 +0700

    Debug: mapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("mysql:mysql-connector-java:8.0.33")
+
+
 
     runtimeOnly("org.postgresql:postgresql")
     compileOnly("org.projectlombok:lombok:$lombokVersion")
